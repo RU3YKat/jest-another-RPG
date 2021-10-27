1ba439b310d61a4e37fa2bc33885e760225e82d4
insomniKat <ru3ykat@gmail.com>
2021-10-27 19:01:05 -0400

    playe getHealth reduceHealth isAlive prototypes pass

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,10 +1,7 @@
-const { test, expect } = require('@jest/globals');
 const Player = require('../lib/Player');
 const Potion = require('../lib/Potion');
 
 jest.mock('../lib/Potion');
-
-console.log(new Potion());
 
 test('creates a player object', () => {
     const player = new Player('Dave');
@@ -37,3 +34,33 @@ test('gets inventory from player or returns false', () => {
 
     expect(player.getInventory()).toEqual(false);
 });
+
+test('gets players health value', () => {
+    const player = new Player('Dave');
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+test('checks if player is alive or not', () => {
+    const player = new Player('Dave');
+
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0;
+
+    expect(player.isAlive()).toBeFalsy();
+});
+
+test('subtracts from players health', () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+
+    expect(player.health).toBe(oldHealth - 5);
+
+    player.reduceHealth(99999);
+
+    expect(player.health).toBe(0);
+});
+
